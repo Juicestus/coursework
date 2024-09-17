f4b3631f891704647fe7f46814d655c6ef2f0399
Juicestus <jus@gtsbr.org>
2024-09-17 17:11:21 -0500

    ENGR 102 finished

--- ENGR-102/lab4/calculate_roots.py
+++ ENGR-102/lab4/calculate_roots.py
@@ -7,30 +7,46 @@
 # Assignment:   Lab 4
 # Date:         12 Sept 2024
 
+import math
+
 def inp(x):
-    return float(input("Please enter the coefficient {x}: "))
+    return float(input(f"Please enter the coefficient {x}: "))
+
+def R(l):
+    n = len(l)
+    if n == 2:
+        return f"The roots are x = {l[0]} and x = {l[1]}"
+    else:
+        return f"The root is x = {l[0]}"
 
 def main():
     a = inp("A")
     b = inp("B")
     c = inp("C")
 
-    r = []
-
-    
-    d = b*b - 4*a*c
-    if d < 0:
-
-
     if a == 0:
+        if b == 0 and c != 0: 
+            return "You entered an invalid combination of coefficients!"
         x = -c/b
-        r.append(x)
+        return R([x])
 
-    if a == 0 and b == 0 and c != 0:
-        print("You entered an invalid combination of coefficients!")
-        return 
-    
 
-    
+    d = b*b - 4*a*c
+    if d > 0:
+        d = math.sqrt(d)
+        x1 = (-b + d) / (2 * a)
+        x2 = (-b - d) / (2 * a)
+        return R([x1, x2])
 
-main()
+    if d == 0:
+        x = (-b + d) / (2 * a)
+        r = f"x = {x}"
+        return R([x])
+
+    else:
+        d = math.sqrt(-d)
+        l = -b / (2 * a) # real
+        i = d / (2 * a)
+        return R([f"{l} + {i}i", f"{l} - {i}i"])
+
+print(main())
