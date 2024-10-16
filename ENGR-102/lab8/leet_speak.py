7795f43c02601de9fbee324c6bc1aba87a16de53
Juicestus <jus@gtsbr.org>
2024-10-16 17:51:30 -0500

    102 lab

---
+++ ENGR-102/lab8/leet_speak.py
@@ -0,0 +1,32 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Justus Languell
+# Section:      201
+# Assignment:   Lab 8 team
+# Date:         15 Oct 2024
+#
+i = input("Enter some text: ")
+print(f'In leet speak, "{i}" is:')
+p = {'a': '4',
+      'e': '3',
+      'o': '0',
+      's': '5',
+      't': '7'}
+for a, b, in p.items():
+    i = i.replace(a, b)
+print(i)
+
+# poopbruh
+# poopbruh
+# poopbruh
+# poopbruh
+# poopbruh
+#whatthe hell
+#whatthe hell
+#whatthe hell
+#whatthe hell
+#whatthe hell
+#whatthe hell
+#whatthe hell
