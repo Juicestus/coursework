ad202d05922a868a6de4a26dd2cfc07b183a30c4
Juicestus <jus@gtsbr.org>
2024-11-01 15:40:46 -0500

    102 Labs + 151 labs

--- ENGR-102/lab10team/no_three_in_a_line.py
+++ ENGR-102/lab10team/no_three_in_a_line.py
@@ -1,41 +1,34 @@
-# By submitting thI assignment, I agree to the following:
+# By submitting this assignment, I agree to the following:
 #   "Aggies do not lie, cheat, or steal, or tolerate those who do."
-#   "I have not given or received any unauthorized aid on thI assignment."
+#   "I have not given or received any unauthorized aid on this assignment."
 #
-# Names:        Justus Languell
+# Name:         Justus Languell
 # Section:      201
-# Assignment:   Lab 7
+# Assignment:   Lab 10
 # Date:         22 Oct 2024
 #
+import itertools
+
+def is_collinear(p1, p2, p3):
+    """Check if three points are collinear using the slope formula."""
+    (x1, y1), (x2, y2), (x3, y3) = p1, p2, p3
+    return (y2 - y1) * (x3 - x1) == (y3 - y1) * (x2 - x1)
+
+def is_valid_placement(points, new_point):
+    """Check if adding new_point keeps all points non-collinear."""
+    for p1, p2 in itertools.combinations(points, 2):
+        if is_collinear(p1, p2, new_point):
+            return False
+    return True
 
 def no_three_in_line(n):
-    I = {k: 0 for k in range(n)}
-    J = {k: 0 for k in range(n)}
-    pts = set()
-    for i in range(n):
-        for j in range(n):
-            if I[i] >= 2: continue
-            if J[j] >= 2: continue
-            I[i] += 1 
-            J[j] += 1 
-            pts.add((i, j))
-    if n % 2 == 1:
-        pts.remove((n-2, n-2))
-    pts.add((n-1, n-2))
-    pts.add((n-2, n-1))
-    return list(pts)
-
-def print_board(n, p):
-    for i in range(n):
-        a = ""
-        for j in range(n):
-            if (i, j) in p:
-                a += "O "
-            else:
-                a += ". "
-        print(a)
+    """Place the maximum number of points on an n x n grid without three in a line."""
+    points = []
+    # Start with the main diagonal to maximize distance between points
+    for x in range(n):
+        for y in range(n):
+            new_point = (x, y)
+            if is_valid_placement(points, new_point):
+                points.append(new_point)
+    return points
             
-
-for i in range(3, 8):
-    print(f'- {i}')
-    print_board(i, no_three_in_line(i))
