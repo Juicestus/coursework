10932989afd3b37a1c932981012e434fbe7a285b
Juicestus <jus@gtsbr.org>
2025-09-14 17:44:46 -0500

    Added comments

--- CS-313/Lab1/buggy.cpp
+++ CS-313/Lab1/buggy.cpp
@@ -15,34 +15,36 @@ class Shape {
 public:
     Shape (int _vertices) {
         vertices = _vertices;
-        points = new Point*[vertices+1];
-        for (int i = 0; i <= vertices; i++)
-            points[i] = new Point();
+        points = new Point*[vertices+1];    // allocating the array of pointers...
+        for (int i = 0; i <= vertices; i++)         
+            points[i] = new Point();        //... but also need memory for each point object 
     }
 
     ~Shape () {
+        // Must deallocated...
         for (int i = 0; i <= vertices; i++)
-            delete points[i];
-        delete[] points;
+            delete points[i];           //... all the individual point memory locations
+        delete[] points;        //... and the actual memory for the array of pointers
     }
 
-    void addPoints(Point* pts) {
+    void addPoints(Point* pts) {    // accepting an array, assumes that the length is at least verticies
         for (int i = 0; i <= vertices; i++) {
             memcpy(points[i], &pts[i%vertices], sizeof(Point));
         }
     }
 
-    double area () {
+    double area () {        // changed return type b/c not returning a pointer anymore
         int temp = 0;
         for (int i = 0; i < vertices; i++) {
             // FIXME: there are two methods to access members of pointers
             //        use one to fix lhs and the other to fix rhs
-            int lhs = points[i]->x * points[i+1]->y;
-            int rhs = (*points[i+1]).x * (*points[i]).y;
+            int lhs = points[i]->x * points[i+1]->y;                // uses arrow operator
+            int rhs = (*points[i+1]).x * (*points[i]).y;            // and also deref + regular accessor (dot operator)
             temp += (lhs - rhs);
         }
         double area = abs(temp)/2.0;
-        return area;
+        return area;                        // not returning the memory location of the area variable
+                                            // because it is stored on the stack
     }
 };
 
@@ -76,7 +78,7 @@ int main () {
     Point quad3;
     quad3.x = 2;
     quad3.y = 2;
-    Point quad4(2, 0);
+    Point quad4(2, 0);              // Reusing the basic compiler b/c of 4 points
 
     Point quadPts[4] = {quad1, quad2, quad3, quad4};
     Shape* quad = new Shape(4);
@@ -86,7 +88,7 @@ int main () {
     std::cout << "Area of triangle = " << tri->area() << "\n";
     std::cout << "Area of quadrilateral = " << quad->area() << "\n";
 
-    delete tri;
+    delete tri;         // deallocate shape objects on the heap
     delete quad;
 
 }
