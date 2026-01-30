01c3f4a45f3d62063c87b6cee6a54a4d07bd9ae1
Juicestus <justus@tamu.edu>
2026-01-29 22:34:07 -0600

    Working

--- CS-438/HW1/client.c
+++ CS-438/HW1/client.c
@@ -2,37 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int
+callback(char** result, struct sockaddr_in* addr)
+{
+	printf("function returned: \"%s\"\n", *result);
+	return 1;
+}
+
 int
 main(int argc, char** argv)
 {
-	CLIENT *clnt;
-	char *host;
-	stuff  out;  /* outgoing parameters */
-	char **result; /* return value */
+	
+	enum clnt_stat stat;
+	stuff out;
+	char* result = 0;
 
-	if(argc < 3) {
-		printf("usage: %s server_host test_string\n", argv[0]);
-		exit(1);
-	}
-	host = argv[1];
-
-	clnt = clnt_create(host, TEST_PROG, TEST_VERS, "tcp");
-	if (clnt == NULL) {
-		clnt_pcreateerror(host);
+	if (argc < 2)
+	{
+		printf("usage: %s test_string\n", argv[0]);
 		exit(1);
 	}
 
-	out.name = argv[2];
+	out.name = argv[1];
 	out.val = 1234;
 
-	result = test_func_1(&out, clnt);	/* call the remote function */
-
-	/* test if the RPC succeeded */
-	if (result == NULL) {
-		clnt_perror(clnt, "call failed:");
+	if ((stat = clnt_broadcast(TEST_PROG, TEST_VERS, TEST_FUNC, (xdrproc_t)xdr_stuff, (caddr_t)&out, 
+			(xdrproc_t)xdr_wrapstring, (caddr_t)&result, (resultproc_t)callback)) != RPC_SUCCESS)
+	{
+		clnt_perrno(stat);
 		exit(1);
 	}
 
-	printf("function returned: \"%s\"\n", *result);
-	clnt_destroy( clnt );
+	return 0;
 }
